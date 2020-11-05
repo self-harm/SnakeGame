62af423d0ff1f62fcb186df289b27e77dc9171dc
self-harm <69676561+self-harm@users.noreply.github.com>
2020-11-05 23:40:45 +0300

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,30 +1,40 @@
-package com.javarush.games.snake;
-
 import com.javarush.engine.cell.*;
-import com.javarush.engine.cell.Game;
 
-public class SnakeGame extends Game{
-
+public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private Snake snake;
+    private int turnDelay;
 
     @Override
-    public void initialize(){
+    public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
     }
 
+    private void createGame(){
+        snake = new Snake(WIDTH/2, HEIGHT/2);
+        drawScene();
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
+        // Apple apple = new Apple(7,7);
+        // apple.draw(this);
+    }
+
     private void drawScene(){
-        for(int width=0; width<WIDTH; width++){
-            for(int height=0;height<HEIGHT;height++){
-                setCellColor(width, height, Color.ANTIQUEWHITE);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellValueEx(i, j, Color.PINK, "");
             }
         }
+        
+        snake.draw(this);
     }
-
-    private void createGame(){
+    
+    @Override
+    public void onTurn(int a){
+        snake.move();
         drawScene();
-
+        
     }
-
 }
