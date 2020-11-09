15ab8ef14dfca9b76c29cff57f9c51fc2888d13f
self-harm <69676561+self-harm@users.noreply.github.com>
2020-11-09 22:43:38 +0300

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,10 +1,12 @@
 import com.javarush.engine.cell.*;
 
+
 public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
     private Snake snake;
     private int turnDelay;
+    private Apple apple;
 
     @Override
     public void initialize() {
@@ -14,9 +16,12 @@ public class SnakeGame extends Game {
 
     private void createGame(){
         snake = new Snake(WIDTH/2, HEIGHT/2);
+        createNewApple();
         drawScene();
         turnDelay = 300;
         setTurnTimer(turnDelay);
+
+
         // Apple apple = new Apple(7,7);
         // apple.draw(this);
     }
@@ -29,12 +34,41 @@ public class SnakeGame extends Game {
         }
         
         snake.draw(this);
+        apple.draw(this);
+    }
+
+    private void createNewApple(){
+        Apple apple = new Apple(getRandomNumber(WIDTH), getRandomNumber(HEIGHT));
+        this.apple=apple;
     }
     
     @Override
     public void onTurn(int a){
-        snake.move();
+        snake.move(apple);
+        if(apple.isAlive==false){
+            createNewApple();
+        }
         drawScene();
-        
+    }
+
+    @Override
+    public void onKeyPress(Key key){
+        switch (key) {
+            case LEFT:
+                snake.setDirection(Direction.LEFT);
+                break;
+
+            case RIGHT:
+                snake.setDirection(Direction.RIGHT);
+                break;
+
+            case UP:
+                snake.setDirection(Direction.UP);
+                break;
+
+            case DOWN:
+                snake.setDirection(Direction.DOWN);
+                break;
+        }
     }
 }
